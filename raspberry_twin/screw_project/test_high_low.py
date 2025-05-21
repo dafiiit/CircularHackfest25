ce5df7bea4f68014db06d8e1faca42da0ff91364
leopoldforkl <leopoldforkl099@gmail.com>
2025-05-21 19:15:08 +0200

    high-low switching

--- raspberry_twin/screw_project/test_high_low.py
+++ raspberry_twin/screw_project/test_high_low.py
@@ -1,11 +1,24 @@
-from hardware_interface.pwm import PWMChannel
+import socket
 import time
 
-pwm_motor = PWMChannel(chip=0, channel=1, gpio_pin=12, frequency_hz=50)
-pwm_motor.enable()
+def set_pin(state: bool):
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.connect("/tmp/gpio_socket")
+    sock.sendall(b"ON" if state else b"OFF")
+    sock.close()
 
-while True:
-    pwm_motor.set_duty_cycle_percent(100)
-    time.sleep(1.0)
-    pwm_motor.set_duty_cycle_percent(0)
-    time.sleep(1.0)
+# Usage:
+try:
+    while True:
+        # Turn LED on
+        set_pin(True)  # Turns pin HIGH
+        time.sleep(1.0)  # LED on for 1 second
+        # Turn LED off
+        set_pin(False)  # Turns pin HIGH
+        time.sleep(1.0)  # LED off for 1 second
+        print("alive")
+except KeyboardInterrupt:
+    # Exit the loop on CTRL+C
+    pass
+finally:
+    set_pin(False)  # Turns pin HIGH
